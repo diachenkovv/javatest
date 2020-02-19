b1743b5f480262100608c3150b1a7079523424a9
Diachenko Viktor <56164898+diachenkovv@users.noreply.github.com>
2020-02-19 16:39:19 +0200

    Практична №3 (завдання 2)
    Co-authored-by: MichaelKhokh96 <michaelkhokh96@gmail.com>

--- style.js
+++ style.js
@@ -1,37 +1,12 @@
-let newArray = new Array(2),i,j;
-function Massiv(newArray) {
-    
-    for ( i = 0; i < 2; i++) {
-        newArray [i] = new Array(2);
-        for (j = 0; j < 2; j++) {
-            newArray [i][j] = i + j;
-        }
+let counter = 0;
+let i = 0;
+function counter1() {
+    counter++;
+}
+while (i < 5) {
+    counter1();
+    i++;
+}
+function alert1() {
+alert(counter);
     }
-}
-Massiv(newArray);
-alert(newArray[0][0]);
-function Show(Mas) {
-    
-    for (let i = 0; i < Mas.length; i++) {
-        
-        for (let j = 0; j < Mas[i].length; j++) {
-            document.write (Mas[i][j]+"   ");
-        }
-        document.write("<br>");
-    }
-}
-
-Show(newArray);
-//arrayTable();
-function arrayTable (rows, cols, DefaultValue){
-var Mas = new Array (rows);
-for (var i = 0; i < rows; i++){
-    Mas [i] = new Array (cols);
-    for (var j=0; j < cols; j++){
-        Mas [i][j] = defaultValue;
-    }
-}
-return Mas;
-}
-let k=new Array(5,1,2,3);
-document.write(k[0]);
