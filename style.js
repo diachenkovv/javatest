ef181367fa22c0ecedea081d3663de364f3434dd
Diachenko Viktor <56164898+diachenkovv@users.noreply.github.com>
2020-02-19 17:09:41 +0200

    Практична №3 (завдання №3)

--- style.js
+++ style.js
@@ -1,12 +1,19 @@
-let counter = 0;
-let i = 0;
-function counter1() {
-    counter++;
+function scale() {
+    let img = document.getElementById("img");
+    if (img.style.width == 100+"px")
+        img.style.width = 500+"px";
+    else
+        img.style.width = 100+"px";
 }
-while (i < 5) {
-    counter1();
-    i++;
-}
-function alert1() {
-alert(counter);
+function scale1() {
+    let sc = 1;
+    let img1 = document.getElementById("img2");
+    if (img1.style.width == 100+"px" && sc == 1) {
+        img1.style.width = 1000+"px";
+        sc = 0;
     }
+    
+    else
+        img1.style.width = 100+"px";
+        
+}
