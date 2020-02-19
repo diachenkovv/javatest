33ada903a9d12cf47fe1f6c669fa6b25bf7409f7
Diachenko Viktor <56164898+diachenkovv@users.noreply.github.com>
2020-02-19 16:20:41 +0200

    Практична №3 (завдання 1) - виправлення

--- style.js
+++ style.js
@@ -1,14 +1,28 @@
-function Massiv() {
-    let newArray = new Array(2);
-    for (let i = 0; i < 3; i++) {
+let newArray = new Array(2),i,j;
+function Massiv(newArray) {
+    
+    for ( i = 0; i < 2; i++) {
         newArray [i] = new Array(2);
-        for (let j = 0; j < 3; j++) {
-            newArray [i] [j] = new Array(2);
+        for (j = 0; j < 2; j++) {
+            newArray [i][j] = i + j;
         }
     }
 }
-Massiv();
-arrayTable();
+Massiv(newArray);
+alert(newArray[0][0]);
+function Show(Mas) {
+    
+    for (let i = 0; i < Mas.length; i++) {
+        
+        for (let j = 0; j < Mas[i].length; j++) {
+            document.write (Mas[i][j]+"   ");
+        }
+        document.write("<br>");
+    }
+}
+
+Show(newArray);
+//arrayTable();
 function arrayTable (rows, cols, DefaultValue){
 var Mas = new Array (rows);
 for (var i = 0; i < rows; i++){
@@ -19,3 +33,5 @@ for (var i = 0; i < rows; i++){
 }
 return Mas;
 }
+let k=new Array(5,1,2,3);
+document.write(k[0]);
