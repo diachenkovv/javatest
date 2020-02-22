eb21a713d87c98b881448624c7f23ea6c5e6d717
Diachenko Viktor <56164898+diachenkovv@users.noreply.github.com>
2020-02-22 14:10:17 +0200

    Практична №4 (завдання 4)

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-function Math1() {
-    let select = document.getElementById("select");
-    let number = document.getElementById("num").value;
-    let value = select.options[select.selectedIndex].value;
-    let res = 0;
-    if (value == 1) {
-        res = Math.sin(number);
-    }
-    else {
-        res = Math.cos(number);
-    }
+// function get_current_age() {
+//     return ((new Date().getTime() - new Date(date)) / (24 * 3600 * 365.25 * 1000)) | 0;
+//     alert(get_current_age('1986-09-18'));
+// }
+function chechData() {
+    var data = prompt('Enter birth')
 
-    alert(res);
+    if (data.match(/^[0-9]{2,2}[\.][0-9]{2,2}[\.][0-9]{4,4}$/i)) {
+        var dateArray = data.split('.');
+        var date = new Date();
+
+        var day = date.getDate() - dateArray[0] ;
+        var month = date.getMonth() + 1 - dateArray[1];
+        var year = date.getFullYear() - dateArray[2];
+
+        var sec = (day * 24 * 60 * 60) + (month * 30 * 24 * 60 * 60) + (year * 12 * 30 * 24 * 60 * 60);
+        var hours = (day * 24) + (month * 30 * 24) + (year * 12 * 30 * 24);
+        var days = (day) + (month * 30) + (year * 12 * 30);
+        sec = sec + (date.getHours() * 60) + date.getSeconds();
+        hours = hours + date.getHours();
+
+        alert('Вы прожили ' + sec + ' секунд ' + hours + ' часов ' + days + ' дней');
+    } else {
+        alert('Ошибка брат ;[');
+    }
 }
